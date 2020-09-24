77b4bdd78e354873ba1c18a6ea3dde7a0df970b7
cypherdare <contact@cyphercove.com>
2020-09-23 20:18:47 -0400

    Add tool tip explanations of color spaces

--- core/src/main/java/com/cyphercove/colorinterpolationcomparison/ColorInterpolationComparison.java
+++ core/src/main/java/com/cyphercove/colorinterpolationcomparison/ColorInterpolationComparison.java
@@ -11,15 +11,16 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.ObjectMap;
-import com.badlogic.gdx.utils.OrderedMap;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.cyphercove.gdxtween.graphics.ColorSpace;
 import com.cyphercove.gdxtween.graphics.GtColor;
 import com.kotcrab.vis.ui.VisUI;
-import com.kotcrab.vis.ui.widget.color.BasicColorPicker;
 import com.kotcrab.vis.ui.widget.color.ColorPickerAdapter;
+
+import static com.badlogic.gdx.math.Interpolation.fade;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class ColorInterpolationComparison extends ApplicationAdapter {
@@ -35,18 +36,39 @@ public class ColorInterpolationComparison extends ApplicationAdapter {
     PlatformResolver platformResolver;
     boolean isDarkBackground = true;
 
-    private static final ObjectMap<String, ColorSpace> choices = new OrderedMap<>();
-    static {
-        choices.put("RGB", ColorSpace.Rgb);
-        choices.put("Linear RGB", ColorSpace.DegammaRgb);
-        choices.put("Lab", ColorSpace.DegammaLab);
-        choices.put("LMS Compressed", ColorSpace.DegammaLmsCompressed);
-        choices.put("IPT", ColorSpace.DegammaIpt);
-        choices.put("Lch", ColorSpace.DegammaLch);
-        choices.put("HSL", ColorSpace.Hsl);
-        choices.put("HCL", ColorSpace.Hcl);
-        choices.put("HSV", ColorSpace.Hsv);
+    private static class Item {
+        String label;
+        ColorSpace colorSpace;
+        String toolTip;
+
+        public Item(String label, ColorSpace colorSpace, String toolTip) {
+            this.label = label;
+            this.colorSpace = colorSpace;
+            this.toolTip = toolTip;
+        }
     }
+
+    private static final Item[] items = {
+            new Item("RGB", ColorSpace.Rgb, "Directly interpolates the gamma-corrected RGB values. aka sRGB."),
+            new Item("Linear RGB", ColorSpace.DegammaRgb, "Interpolates in linear RGB space, so gamma " +
+                    "correction is removed. The blend is even in terms of light energy, but it does not appear even to the eye."),
+            new Item("Lab", ColorSpace.DegammaLab, "Interpolates in CIELAB color space, which was designed " +
+                    "for visually even intensity changes. Some color combinations may produce faint extra hues in the middle of the interpolation."),
+            new Item("LMS Compressed", ColorSpace.DegammaLmsCompressed, "Interpolates in LMS space after " +
+                    "apply a gamma compression to produce extremely visually even blends. This space is and intermediate " +
+                    "stage of transforming an RGB value to IPT space."),
+            new Item("IPT", ColorSpace.DegammaIpt, "Interpolates in IPT space, which produces extremely " +
+                    "visually even blends."),
+            new Item("Lch", ColorSpace.DegammaLch, "Interpolates in Lch space, a cylindrical transformation" +
+                    " of CIELAB space. Hue is one of the dimensions, so it can produce unrelated intermediate hues."),
+            new Item("HSL", ColorSpace.Hsl, "Interpolates in HSL space. This can produce unrelated " +
+                    "intermediate hues. Since saturation is independent of brightness in the definition of HSL, bright " +
+                    "colors can appear when blending between almost-white colors and gray-ish or dark colors."),
+            new Item("HCL", ColorSpace.Hcl, "Interpolates in HSL space using chroma instead of saturation, " +
+                    "which avoids the issue of unwanted intermediate bright color when blending between almost-white" +
+                    "colors and gray-ish or dark colors."),
+            new Item("HSV", ColorSpace.Hsv, "Interpolates in HSV space. This can produce unrelated intermediate hues.")
+    };
 
     public ColorInterpolationComparison() {
         this(null);
@@ -89,10 +111,32 @@ public class ColorInterpolationComparison extends ApplicationAdapter {
         });
         table.add(firstColorPicker).center().pad(20);
 
+        TooltipManager tooltipManager = new TooltipManager() {
+            {
+                initialTime = 0.15f;
+                resetTime = 0.5f;
+                offsetX = -230;
+                hideAll();
+            }
+            @Override
+            protected void showAction (Tooltip tooltip) {
+                float actionTime = 0.25f;
+                Container<?> container = tooltip.getContainer();
+                container.setTransform(true);
+                container.getColor().a = 0.2f;
+                container.setScale(0.05f);
+                container.addAction(parallel(fadeIn(actionTime, fade), scaleTo(1, 1, actionTime, fade)));
+            }
+        };
+        VisUI.getSkin().get("default", TextTooltip.TextTooltipStyle.class).wrapWidth = 200;
+        VisUI.getSkin().get("default", TextTooltip.TextTooltipStyle.class).label.fontColor = Color.WHITE;
         Table innerTable = new Table(VisUI.getSkin());
-        for (ObjectMap.Entry<String, ColorSpace> entry : choices) {
-            innerTable.add(entry.key).center();
-            innerTable.add(new ColorTransition(entry.value)).growX().height(30).space(10);
+        for (Item item : items) {
+            Label label = new Label(item.label, VisUI.getSkin());
+            label.addListener(new TextTooltip(item.toolTip, tooltipManager, VisUI.getSkin()));
+            label.setAlignment(Align.center);
+            innerTable.add(label).center().fill();
+            innerTable.add(new ColorTransition(item.colorSpace)).growX().height(30).space(10);
             innerTable.row();
         }
         table.add(innerTable).grow();
